6965f24cc30d88db5b4d5a294bb31cb18bf45dc7
Ali Sleiman <asleiman@acksession.com>
2025-05-20 23:30:07 +0300

    IT upload POS, delete and update

--- src/apiMAG/students.ts
+++ src/apiMAG/students.ts
@@ -1,4 +1,4 @@
-import { PlanOfStudy } from '../pages/30/Students';
+import { PlanOfStudy, PlanOfStudyGet } from '../pages/30/Students';
 import api from './api';
 
 export const uploadPOS = async (pos: PlanOfStudy): Promise<string> => {
@@ -7,6 +7,24 @@ export const uploadPOS = async (pos: PlanOfStudy): Promise<string> => {
         return response.data.message;
     } catch (error: any) {
         const errorMessage = error.response?.data?.message || 'Upload failed';
+        throw new Error(errorMessage);
+    }
+};
+export const DeletePOS = async (depId :number): Promise<string> => {
+    try {
+        const response = await api.post('/remove_pos', { "departmentid": depId});
+        return response.data.message;
+    } catch (error: any) {
+        const errorMessage = error.response?.data?.message || 'Delete failed';
+        throw new Error(errorMessage);
+    }
+};
+export const GetDepartmentsPOS = async (): Promise<PlanOfStudyGet[]> => {
+    try {
+        const response = await api.get('/getAllDepartmentsPOS');
+        return response.data;
+    } catch (error: any) {
+        const errorMessage = error.response?.data?.message || 'Request failed';
         throw new Error(errorMessage);
     }
 };
